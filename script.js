70454f880804227ed79f9efb1f40d390c32489d0
kilbergr <rekilberg@gmail.com>
2015-05-27 11:46:46 -0700

    working model

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 
 $(document).mousemove(function(e){
-    $("#puppy").stop().animate({left:e.pageX, top:e.pageY});
+    $("#hand").stop().animate({left:e.pageX, top:e.pageY});
 });
 
-$("#puppy").on("click", function(e){
+$("#hand").on("click", function(e){
+	//Show background
 	$(".bg").css("visibility", "visible");
-	var x = e.pageX;
-	var y = e.pageY;
-	$("#paw").addClass('animated bounceInLeft');
-	if($("#paw").hasClass('animated bounceInLeft')){
-		$("#puppy").addClass('animated wobble');
-	}
+	//Determine where was clicked and have new div set to appear in that location
+	var x = e.pageX-600 + "px";
+	var y = e.pageY + "px";
+	var div = $('<div>').css({
+		"position": "absolute",
+		"left": x,
+		"top": y
+	});
+	div.append($("#paw"))
+	$(document.body).append(div);
+	//Animations
+	$("#paw").addClass('animated bounceInLeft').addClass('animated bounceOutLeft');
+	$("#hand").addClass('animated wobble');
+	//Audio
+        var smackEl = document.createElement('audio');
+        smackEl.setAttribute('src', 'Smack.mp3');
+        smackEl.setAttribute('autoplay', 'autoplay');
+        //smackEl.load()
+        $.get();
+        smackEl.addEventListener("load", function() {
+        smackEl.play();
+        }, true);
 
+        $('.play').click(function() {
+        smackEl.play();
+        });
+
+	//Hide background
+		setTimeout(function(){
+		$(".bg").css("visibility", "hidden")
+	}, 600);
 })
-
